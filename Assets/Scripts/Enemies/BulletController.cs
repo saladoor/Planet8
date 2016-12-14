38f1a61dae331e59fd18eee92dba8113343ec793
saladoor <sghali@hotmail.com>
2016-12-14 12:40:08 +0100

    Scripts in folders

---
+++ Assets/Scripts/Enemies/BulletController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public CircleCollider2D cc;
+	public float bulletSpeed = 2.0F;
+
+	public void shoot(Vector2 move)
+	{
+		rb.velocity = move.normalized * bulletSpeed;
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(!(col.tag == "Player" || col.tag == "slowmo"))
+		{
+			cc.transform.position = new Vector3(0f, 15f, 0f);
+			rb.velocity = new Vector3(0f, 0f, 0f);
+			//Destroy(this.gameObject);
+		}
+	}
+}
